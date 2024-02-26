c409464ea597f96a6d65f6a699f597d30d28fd91
Mickael Riss <mendos.prod@gmail.com>
2024-02-25 23:52:25 -0500

    Add color title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,8 @@
 export default function Title({ children }: { children: React.ReactNode }) {
   return (
-    <h1 className="text-3xl md:text-4xl xl:text-6xl font-bold text-center">
+    <h1 className="text-3xl md:text-4xl xl:text-6xl font-bold text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent text-stroke py-2"
+    aria-label="Crypto Tracking and Investigation Platform"
+    >
       {children}
     </h1>
   );
